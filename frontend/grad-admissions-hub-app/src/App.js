79fedbb2b1a9afd1c2abe7c510b93ee8ce6d4f3f
Roustom <47872392+Roustom@users.noreply.github.com>
2021-03-12 09:20:29 -0500

    set up apollo client and fetch data and display  it
    Co-authored-by: Leo <leo-paz@users.noreply.github.com>

--- frontend/grad-admissions-hub-app/src/App.js
+++ frontend/grad-admissions-hub-app/src/App.js
@@ -4,18 +4,24 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Navbar from './Components/Navbar/Navbar';
+import Provider from './Providers/ApolloProvider';
+import { ApolloProvider } from '@apollo/client';
+
 
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar/>
-        <Switch>
-          <Route path="/about" component={About} />
-          <Route path="/" exact component={Home} />
-        </Switch>
-      </div>
+      {/* <Provider> */}
+        <div>
+          <Navbar/>
+          <Switch>
+            <Route path="/about" component={About} />
+            <Route path="/" exact component={Home} />
+          </Switch>
+        </div>
+        {/* </Provider> */}
     </Router>
+    
   );
 }
 
--- frontend/grad-admissions-hub-app/src/Pages/Home.js
+++ frontend/grad-admissions-hub-app/src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
+import GetBook1 from './../Components/GetBook1';
 
 const Home = () => {
 
     return ( 
-        <div className="page">
-            <h1>Home</h1>
-        </div>
+            <div className="page">
+                <h1>Home</h1>
+                <GetBook1></GetBook1>
+            </div>        
     )
 }
 
