c319fbbee16a417541339eeeebb070d3851c79d0
Roustom <47872392+Roustom@users.noreply.github.com>
2021-03-12 09:44:26 -0500

    clean up code and get rid on console warnings

--- frontend/grad-admissions-hub-app/src/App.js
+++ frontend/grad-admissions-hub-app/src/App.js
@@ -1,27 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
-import Navbar from './Components/Navbar/Navbar';
-import Provider from './Providers/ApolloProvider';
-import { ApolloProvider } from '@apollo/client';
-
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <Router>
-      {/* <Provider> */}
-        <div>
-          <Navbar/>
-          <Switch>
-            <Route path="/about" component={About} />
-            <Route path="/" exact component={Home} />
-          </Switch>
-        </div>
-        {/* </Provider> */}
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </div>
     </Router>
-    
   );
 }
 
--- frontend/grad-admissions-hub-app/src/index.js
+++ frontend/grad-admissions-hub-app/src/index.js
@@ -1,63 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { gql } from '@apollo/client';
-import reportWebVitals from './reportWebVitals';
-import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from} from '@apollo/client';
-import {onError} from '@apollo/client/link/error';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 
-
-
-const errorLink = onError(({graphqlErrors, networkError}) => {
-  if(graphqlErrors) {
-      graphqlErrors.map(({message, location, path}) => {
-          alert(`Graphql error ${message}`);
-      });
-
-      
+/**
+ * Detects errors. This will be optimized to better catch errors in the future.
+ */
+const errorLink = onError(({ graphqlErrors }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+      alert(`Graphql error location: ${location}`);
+      alert(`Graphql error path: ${path}`);
+      return message;
+    });
   }
-  // if(networkError){
-  //   networkError.map(({message, location, path}) => {
-  //     alert(`network error ${message}`);
-  // });
-  // }
 });
 
 /**
-* Links our database
-*/
+ * Links our database
+ */
 const dbLink = from([
   errorLink,
-  new HttpLink({ uri:"https://j2ofh2owcb.execute-api.us-east-1.amazonaws.com/dev/graphql"})
-])
-
- const client = new ApolloClient({
+  new HttpLink({
     uri: "https://j2ofh2owcb.execute-api.us-east-1.amazonaws.com/dev/graphql",
-    cache: new InMemoryCache(),
-    link: dbLink,
-    headers: {
-    },
-    
- });
+  }),
+]);
 
-//  client
-//   .query({
-//     query: gql`
-//      {
-//       bookById(id: "book-2"){
-//         id
-//         name
-//         pageCount
-//         author {
-//           firstName
-//           lastName
-//         }
-//       }
-//     }
-//     `
-//   })
-//   .then(result => console.log(result));
+/**
+ * Initalizes out Client for the provider
+ */
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: dbLink,
+  headers: {},
+});
 
 ReactDOM.render(
   <React.StrictMode>
@@ -65,7 +51,7 @@ ReactDOM.render(
       <App />
     </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
