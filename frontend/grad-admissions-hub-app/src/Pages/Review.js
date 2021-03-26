e6ce5af633bf3e26fb95151a784c784025a675fb
Adam Labelle <imadamlabelle@gmail.com>
2021-03-25 23:28:49 -0400

    Added Submission and Review Application Page

---
+++ frontend/grad-admissions-hub-app/src/Pages/Review.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import GetApplication from "./../Components/GetApplication";
+
+const Review = () => {
+
+    return ( 
+        <div className="page">
+            <h1>Review Application</h1>
+            <h2>Here is where we will display the Applicants File</h2>
+            <GetApplication></GetApplication>
+        </div>
+    )
+}
+
+export default Review;
---
+++ frontend/grad-admissions-hub-app/src/Pages/Submission.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import DataFetch from '../Components/DataFetch';
+
+const Submission = () => {
+
+    return ( 
+        <div className="page">
+            <h1>Submission Page</h1>
+            <h2>Enter your information below</h2>
+            <DataFetch></DataFetch>
+        </div>
+    )
+}
+
+export default Submission;
