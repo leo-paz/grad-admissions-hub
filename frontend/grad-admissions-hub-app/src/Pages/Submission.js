b3fe35db2ccff843684c157e59ce0f59d5610c46
Roustom <47872392+Roustom@users.noreply.github.com>
2021-04-08 23:25:29 -0400

    get applications for the user from the database

--- frontend/grad-admissions-hub-app/src/Pages/Submission.js
+++ frontend/grad-admissions-hub-app/src/Pages/Submission.js
@@ -5,7 +5,70 @@ import { Link } from 'react-router-dom';
 import Button from '../Components/Button/Button';
 
 import { UserContext } from '../App';
+import { useQuery, gql } from '@apollo/client';
 
+function getUserApplicationsQuery(id, profile) {
+    if (profile === 'professor') {
+        const PROF_QUERY = gql`
+            {
+                professorById(id: "${id}") {
+                    applications {
+                        id
+                        areasOfResearch
+                    }
+                }
+            }`
+        ;
+        return PROF_QUERY;
+    }
+
+    const APPLICANT_QUERY = gql`
+        {
+            applicantById(id: "${id}") {
+                applications {
+                    id
+                    areasOfResearch
+                }
+            }
+        }`
+    ;
+    return APPLICANT_QUERY;
+}
+
+
+
+// function getApplicationQuery(id){
+
+//     const REV_QUERY = gql`
+//     {
+//         applicationById(id: "b0071922-6184-480a-a5db-a0897ed85ae3") {
+//             applicant {
+//                 id
+//                 name
+//             }
+//             professor {
+//                 id
+//                 name
+//             }
+//             dateSubmitted
+//             areasOfResearch
+//             resumeDocumentId
+//             diplomaDocumentId
+//             auditDocumentId
+//             reviews {
+//                 id
+//                 title
+//                 dateSubmitted
+//                 ranking
+//                 body
+//             }
+//         }
+//     }
+
+//     `;
+
+//     return REV_QUERY;
+// }
 const orgApplications = [
     {
         areasOfResearch: ['Biology', 'Neuroscience', 'Quiditch'],
@@ -27,21 +90,29 @@ const orgApplications = [
 
 const Submission = () => {
     const { userState } = useContext(UserContext);
-    const [applications, setApplications] = useState(orgApplications);
-
-    useEffect(() => {
-        async function getApplicationsForUser() {
-            try {
-                // Get current 
-                // TODO: Call backend with apollo client here
-                // setApplications(res)
-            } catch (e) {
-                console.log(e);
-            }
-        }
-        getApplicationsForUser();
-    });
-
+    // const [applications, setApplications] = useState(orgApplications);
+    const { loading, error, data } = useQuery(getUserApplicationsQuery(userState.id,userState.profile),{returnPartialData:true});
+    
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+    console.log(data)
+    // if (loading) return 'Loading...';
+    // if (error) return `Error! ${error.message}`;
+    // console.log(data && data.applicationById.reviews);
+    // useEffect(() => {
+    //     async function getApplicationsForUser() {
+    //         try {
+    //             // Get current 
+    //             // TODO: Call backend with apollo client here
+    //             // setApplications(res)
+    //         } catch (e) {
+    //             console.log(e);
+    //         }
+    //     }
+    //     getApplicationsForUser();
+    // });
+    const applications = (userState.profile === "applicant") ? data.applicantById.applications: data.professorById.applications;
+    
     return (
         <div className="center-container">
             {userState.profile === 'applicant' && (<CreateApplication applicantId={userState.email}/>)}
